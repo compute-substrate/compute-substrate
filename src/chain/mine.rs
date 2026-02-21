7b2593e860e0292053a5dab5a9bb9de1f6ac479c
compute-substrate <computesubstrate@outlook.com>
2026-02-21 12:58:56 +0100

    Update mine.rs

--- src/chain/mine.rs
+++ src/chain/mine.rs
@@ -57,7 +57,9 @@ fn merkle_root(txs: &[Transaction]) -> Hash32 {
 pub fn coinbase(miner_h160: Hash20, value: u64, height: u64, memo: Option<&[u8]>) -> Transaction {
     let script_sig = height.to_le_bytes().to_vec();
     let locktime = height as u32;
-
+    if let Some(m) = memo {
+    script_sig.extend_from_slice(m);
+}
     Transaction {
         version: 1,
         inputs: vec![TxIn {
