92016e07b25a49f6479f9a3b11a4a60f79fa2b86
compute-substrate <computesubstrate@outlook.com>
2026-02-22 22:32:54 +0100

    Update reorg.rs

--- src/chain/reorg.rs
+++ src/chain/reorg.rs
@@ -1005,5 +1005,5 @@ pub fn maybe_reorg_to(db: &Stores, new_tip: &Hash32, mempool: Option<&Mempool>) 
 
     mempool_prune_if_present(db, mempool);
     Ok(())
-}
-                      }
+
+                      
